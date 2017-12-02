604b02442a174a494ce6fe6b322096e38a97ddc5
LemurPwned <jakub.mojsiejuk@gmail.com>
2017-12-02 18:15:12 +0100

    kafka receive and put forward

--- kafka_flow/html_parse.py
+++ kafka_flow/html_parse.py
@@ -6,14 +6,26 @@ import os
 logging.basicConfig(level='INFO')
 consuming_host = '192.168.43.138:9092'
 producing_host = '192.168.43.138:9092'
-#
-# consumer = kafka.KafkaConsumer('Receiver', bootstrap_servers=producing_host)
 
+url_consumer = kafka.KafkaConsumer(bootstrap_servers=producing_host)
+consumer_topic = 'instructions'
+url_consumer.subscribe([consumer_topic])
+url_tab = []
+sent = True
+while sent:
+    for msg in url_consumer:
+        url_tab = str(msg.value)
+        print(url_tab)
+        sent = False
+        break
+url = url_tab.split('|')[0].replace('b\'', '')
+print("\nURL: ",url)
+topic = url_tab.split('|')[1].split(';')[0]
+print("\nTOPIC: ",topic)
 producer = kafka.KafkaProducer(bootstrap_servers=consuming_host)
-URLS = ["https://www.facebook.com/search/4/photos-tagged"]
 mail = "witek1151@wp.pl"
 password = "Wants1^Perfect^Through"
-w_string = "wget \"https://www.facebook.com/search/4/photos-tagged\" --post-data \"email={}&pass={}\" --no-check-certificate --keep-session-cookies --save-cookies=cookies --load-cookies=cookies -U \"Mozilla/5.0 (Windows NT 5.2; rv:2.0.1) Gecko/20100101 Firefox/4.0.1\" -O test".format(mail, password)
+w_string = "wget {} --post-data \"email={}&pass={}\" --no-check-certificate --keep-session-cookies --save-cookies=cookies --load-cookies=cookies -U \"Mozilla/5.0 (Windows NT 5.2; rv:2.0.1) Gecko/20100101 Firefox/4.0.1\" -O test".format(url, mail, password)
 print(w_string)
 os.system(w_string)
 with open('test', 'r') as f:
@@ -28,7 +40,7 @@ counter = 0
 while True:
     bytes_str = r[a:b].encode()
     counter += stride
-    producer.send('nifiTest', bytes_str).get(timeout=30)
+    producer.send(topic, bytes_str).get(timeout=30)
     a = b
     b = b + stride
     if counter >= p:
